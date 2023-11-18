79466ce21a91219101503e8e450a941ec142aa51
lucadev04 <luca07457@gmail.com>
2023-11-18 17:59:20 +0100

    deleted lazarus gui, created mainscreen.ui, added uptime feauture

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtCore import QTimer
+from PyQt6.uic import *
+
 import psutil
 import datetime
-import os
-import time
+import sys
 
-
-while True:
-    f = open('digital_health.txt', 'r')
-    should_time = f.readline(6)
-    f.close()
+def uptime():
     now_time = datetime.datetime.today()
     boot_time = datetime.datetime.fromtimestamp(psutil.boot_time())
     up_time = now_time-boot_time
@@ -15,12 +15,13 @@ while True:
     up_time_h = up_time_2.split(':')[0]
     up_time_min = up_time_2.split(':')[1]
     up_time_sec = up_time_2.split(':')[2]
-    up_time_3 = up_time_h+up_time_min+up_time_sec
-    f = open('digital_health.txt', 'w')
-    f.write(should_time)
-    f.write(str(up_time).split('.')[0])
-    f.close()
-    if int(up_time_3) >= int(should_time):
-        os.system('notify-send Digital-Health "Deine Bildschirmzeit für heute vorbei" --icon=/home/luca/Luca/Privat/Python/digitalhealth/Lazarus_GUI/digital_health.ico')
-        break
-    time.sleep(1)
+    w.lblusetime.setText(up_time_h + ":" + up_time_min + ":" + up_time_sec)
+
+app = QApplication(sys.argv)
+w = loadUi("/home/luca/Luca/Privat/Python/digitalhealth/GUI/mainscreen.ui")
+timer = QTimer()
+timer.timeout.connect(uptime)
+timer.start(1000)
+
+w.show()
+sys.exit(app.exec())
--- test.py
+++
@@ -1,9 +0,0 @@
-numbers = [1, 2, 4, 3, 6, 10, 11, 12, 111, 112, 12345678]
-
-for n in numbers:
-    test = n/2
-    test2 = str(test).split('.')[1]
-    if test2 == "0":
-        print(n)
-    else:
-        continue
