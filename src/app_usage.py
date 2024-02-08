3bdfbb5594dbe5acd27efda633675e15b6a33737
lucadev04 <luca07457@gmail.com>
2024-02-08 17:21:52 +0100

    fixed some bugs to make the script functional

--- src/app_usage.py
+++ src/app_usage.py
@@ -19,7 +19,8 @@ def create_table():
     try:
         formated_date = date_formatter()
         db = sqlite3.connect("../digitalhealth.db")
-        command = """CREATE TABLE IF NOT EXISTS """+formated_date+"""(appname TEXT PRIMARY KEY,
+        command = """CREATE TABLE IF NOT EXISTS """+formated_date+""" (appid INTEGER PRIMARY KEY,
+                                                                appname TEXT,
                                                                 usetime INTEGER,
                                                                 max_usage INTEGER);"""
         c = db.cursor()
@@ -30,13 +31,13 @@ def create_table():
         db.close()
 
 
-def insert_data(appname, usetime, max_usage):
+def insert_data(appid, appname, usetime, max_usage):
     try:
         formated_date = date_formatter()
         db = sqlite3.connect("../digitalhealth.db")
-        command = """INSERT INTO """+formated_date+"""(appname, usetime, max_usage) VALUES(?,?,?)"""
+        command = """INSERT INTO """+formated_date+""" (appid, appname, usetime, max_usage) VALUES(?,?,?,?)"""
         c = db.cursor()
-        c.execute(command, (appname, usetime, max_usage))
+        c.execute(command, (appid, appname, usetime, max_usage))
         db.commit()
     except Error as e:
         print(e)
@@ -44,13 +45,13 @@ def insert_data(appname, usetime, max_usage):
         db.close()
 
 
-def update_data(usetime, appname):
+def update_data(usetime, appid):
     try:
         formated_date = date_formatter()
         db = sqlite3.connect("../digitalhealth.db")
-        command = """UPDATE """+formated_date+""" SET usetime = ? WHERE appname = ?"""
+        command = """UPDATE """+formated_date+""" SET usetime = ? WHERE appid = ?"""
         c = db.cursor()
-        c.execute(command, (usetime, appname))
+        c.execute(command, (usetime, appid))
         db.commit()
     except Error as e:
         print(e)
@@ -62,23 +63,30 @@ def get_apps():
     try:
         formatted_date = date_formatter()
         db = sqlite3.connect('../digitalhealth.db')
-        command = """SELECT appname FROM """+formatted_date
+        command = """SELECT appid FROM """ + formatted_date
         c = db.cursor()
         c.execute(command)
+        applist = []
         apps = c.fetchall()
-        return apps
+        for app in apps:
+            print(app[0])
+            applist.append(app[0])
+        print(applist)
+        return applist
     except Error as e:
         print(e)
     finally:
         db.close()
 
-def get_usetime(appname):
+
+
+def get_usetime(appid):
     try:
         formatted_date = date_formatter()
         db = sqlite3.connect('../digitalhealth.db')
-        command = """SELECT usetime FROM """+formatted_date+""" WHERE appname = ?"""
+        command = """SELECT usetime FROM """+formatted_date+""" WHERE appid = ?"""
         c = db.cursor()
-        c.execute(command, (appname,))
+        c.execute(command, (appid,))
         usetime = c.fetchone()
         print(usetime)
         return usetime
@@ -86,7 +94,6 @@ def get_usetime(appname):
         print(e)
     finally:
         db.close()
-
 
 
 #gets the window which is active and returns the applicationname
@@ -105,10 +112,12 @@ def get_active_app():
 
     return appname
 
+
 def get_value(name):
     with open("ids.json", "r") as f:
         data = json.load(f)
         return data[name]
+
 
 def generate_id(appname):
     id = ""
@@ -119,25 +128,31 @@ def generate_id(appname):
         for l in data:
             if l == letter:
                 id += get_value(l)
-    return id
+    return int(id)
 
 
 
-
-print(generate_id(get_active_app()))
 create_table()
+apps = get_apps()
 # print the name of the active window all 10 seconds and safes the app and usetime into a database
 while True:
     active_app = get_active_app()
-    print("active application:", active_app)
+    active_app_id = generate_id(get_active_app())
+    print("active application:", active_app_id)
     apps = get_apps()
+    print(apps)
     for n in apps:
-        app = str(n).replace('(', '').replace(')', '').replace('"', '').replace(',', '').replace('\\x00', '\x00')
-        print(app)
-        insert_data(str(active_app), 10, 0)
-        usetime = get_usetime(app)
-        usetime_new = usetime+10
-        update_data(usetime_new, n)
+        print(n)
+        print(active_app_id)
+        if n == active_app_id:
+            usetime_old = get_usetime(n)
+            usetime_new = 10
+            usetime = int(usetime_old[0])+usetime_new
+            update_data(usetime, n)
+            print("yes")
+        else:
+            insert_data(active_app_id, active_app, 10, 0)
+            print("no")
     time.sleep(10)
 
 
