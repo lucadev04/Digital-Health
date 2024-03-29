b69cfd8c0c8b0b8710fbe0e91d57facafab491f7
lucadev04 <luca07457@gmail.com>
2024-03-29 15:30:36 +0100

    implemented readDatabase function

--- frontend/lib/dashpage.dart
+++ frontend/lib/dashpage.dart
@@ -3,19 +3,29 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
-import 'package:sqlite3/sqlite3.dart';
+import 'package:sqlite3/sqlite3.dart' as sqlite;
 import 'package:path/path.dart' as p;
 
 class GetData{
-  String get_date(){
+  String getDate(){
     var filePath = p.join('/home/luca/Luca/Privat/Python/digitalhealth', 'config.json');
     File file = File(filePath);
     var fileContent = file.readAsStringSync();
     var data = jsonDecode(fileContent);
     return data['Date'];
   }
-  void read_database(){
-
+  String readDatabase(){
+    final db = sqlite.sqlite3.open('/home/luca/Luca/Privat/Python/digitalhealth/digitalhealth.db');
+    final sqlite.ResultSet usetimes = db.select('SELECT * FROM '+getDate());
+    var apps = Map<String, String>();
+    for (final sqlite.Row row in usetimes) {
+      var appname = row['appname'];
+      var usetime = row['usetime'].toString();
+      apps[appname] = usetime;
+    }
+    var nemo = apps.values.toList();
+    var nemo2 = nemo[2];
+    return nemo2;
   }
 }
 
@@ -38,7 +48,7 @@ class DashPage extends StatelessWidget {
               child: UsetimeWidget(),
             ),
             Card(
-              child: Text(GetData().get_date()),
+              child: Text(GetData().readDatabase()),
             ),
             Card()
           ],
